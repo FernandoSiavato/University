69560801e1e6d35f0604c88436ce7fd877b49c73
FernandoSiavato <fernando.siavato@gmail.com>
2022-07-25 01:40:52 -0400

    Editando el proyecto y probando los ejercicio

---
+++ Ejercicio/Untitled-1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main (){
+    int a = 2, c=1, aux=0,aux2=0;
+    float b = c-1+a, aux3=0;
+    
+    // aux = b * b > 4 ||  pow(a,4) > c * c + 20;
+    // aux = 1 + 15 / pow(2,3) / b;
+    //aux2 = b * (a+1) - b/a/10;
+    // aux2 = c * (5%3); */;
+
+    cout<<aux2<<endl;
+    cout<<aux3;
+
+}
--- Proyecto 1/Proyecto 1.cpp
+++ Proyecto 1/Proyecto 1.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 main(){
-    int iniciaMOX=0,iniciaMOY=0,sentidoMOX=0,sentidoMOY=0; //Las coordenadas de inicio
-    int cInteraccion=0,interaccionX=0,interacionY=0;
-    // int coordY=0, coordX=0;
+    int iniciaMOX=0,iniciaMOY=0,sentidoMOX=0,sentidoMOY=0; // Mario Origital, Las coordenadas de inicio y su sentidos.
+    int cInteraccion=0,interaccionX=0,interacionY=0; // Cantidad de interacciones y la interacci[on en x y y.
+    int sentidoMC1X=0, sentidoMC1Y=0 ,sentidoMC2X=0, sentidoMC2Y=0; // Sentido de copia 1 y 2
+    // int cMoster=0, moster;
+    
 
     cout<<"Digite la posicion inicial de Mario Original ";
     cin>>iniciaMOX>>iniciaMOY;
@@ -13,9 +15,39 @@ main(){
     cout<<"Digite el sentido de Mario Original ";
     cin>>sentidoMOX>>sentidoMOY;
 
+    cout<<"Digite el sentido de la 1ra copia de Mario ";
+    cin>>sentidoMC1X>>sentidoMC1Y;
+
+    cout<<"Digite el sentido de la 2da copia de Mario ";
+    cin>>sentidoMC2X>>sentidoMC2Y;
+
+
     //Sentido del Mario Original en la posicion inicial
-    iniciaMOX = iniciaMOX*(sentidoMOX);
-    iniciaMOX = iniciaMOY*(sentidoMOY);
+    iniciaMOX *= (sentidoMOX);
+    iniciaMOY *= (sentidoMOY);
+
+    
+    if((iniciaMOX == 0) && (iniciaMOY == 0) ){ // Si introduce esto debe salir algun error o hasta parar el programa.
+		"cuadrante errado";
+	}
+    else if((iniciaMOX >= 0) && (iniciaMOY >= 0)){ //"Cuadrante 1", el sentido del movimiento de las copias no cambiará.
+	}
+    else if((iniciaMOX < 0) && (iniciaMOY >= 0)){ // "Cuadrante 2", el sentido del movimiento de las copias cambiará en el eje X.
+        sentidoMC2X *= (-1);
+        sentidoMC1X *= (-1);
+	}
+    else if((iniciaMOX < 0) && (iniciaMOY < 0)){// "Cuadrante 3", el sentido del movimiento de las copias cambiará en el eje Y.
+		sentidoMC2Y *= (-1);
+        sentidoMC1Y *= (-1);
+	}
+    else if((iniciaMOX >= 0) && (iniciaMOY < 0)){ // "Cuadrante 4", el sentido del movimiento de las copias cambiará en el eje X y Y.
+		sentidoMC1X *= (-1);
+        sentidoMC2Y *= (-1);
+        sentidoMC1Y *= (-1);
+        sentidoMC2X *= (-1);
+	}else{
+        cout<<"No hay sentido";
+    }
     
     cout<<"Digite cantidad de interacciones ";
     cin>>cInteraccion;
@@ -23,8 +55,8 @@ main(){
     for(int i = 0;i < cInteraccion;i++){
             cout<<"Diga las lineas de Movimiento del Maria Original "<<"["<<i<<"] ";
             cin>>interaccionX>>interacionY;
-            iniciaMOX=iniciaMOX+(interaccionX*(sentidoMOX));
-            iniciaMOY=iniciaMOY+(interacionY*(sentidoMOY));
+            iniciaMOX+=(interaccionX*(sentidoMOX));
+            iniciaMOY+=(interacionY*(sentidoMOY));
         }
 
         cout<<"posicion "<<iniciaMOX<<" "<<iniciaMOY<<" "<<endl; 
